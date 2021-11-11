d9e254d77a4ad1d3127dfb1ffb0553797cb89e92
y-uchiida <y.uchiida@nirneege.com>
2021-11-12 02:03:27 +0900

    fix web.php(typo)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\HomeroomController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,35 +21,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (){ return (redirect('/student')); });
 
 /* students に関するルーティング */
-Route::get('/student', [StudentControoler::class, 'index'])->name('student');
-Route::get('/student/add', [StudentControoler::class, 'add']);
-Route::post('/student/add', [StudentControoler::class, 'create']);
-Route::post('/student/{id}', [StudentControoler::class, 'update']);
-Route::delete('/student/{id}', [StudentControoler::class, 'delete']);
-Route::get('/student/{id}', [StudentControoler::class, 'edit']);
+Route::get('/student', [StudentController::class, 'index'])->name('student');
+Route::get('/student/add', [StudentController::class, 'add']);
+Route::post('/student/add', [StudentController::class, 'create']);
+Route::post('/student/{id}', [StudentController::class, 'update']);
+Route::delete('/student/{id}', [StudentController::class, 'delete']);
+Route::get('/student/{id}', [StudentController::class, 'edit']);
 
 /* teachers に関するルーティング */
-Route::get('/teacher', [TeacherControoler::class, 'index'])->name('teachers');
-Route::get('/teacher/add',[TeacherControoler::class, 'add']);
-Route::post('/teacher/add',[TeacherControoler::class, 'create']);
-Route::get('/teacher/{id}' ,[TeacherControoler::class, 'edit']);
-Route::post('/teacher/{id}' ,[TeacherControoler::class, 'update']);
-Route::delete('/teacher/{id}' ,[TeacherControoler::class, 'delete']);
+Route::get('/teacher', [TeacherController::class, 'index'])->name('teachers');
+Route::get('/teacher/add',[TeacherController::class, 'add']);
+Route::post('/teacher/add',[TeacherController::class, 'create']);
+Route::get('/teacher/{id}' ,[TeacherController::class, 'edit']);
+Route::post('/teacher/{id}' ,[TeacherController::class, 'update']);
+Route::delete('/teacher/{id}' ,[TeacherController::class, 'delete']);
 
 /* subjects に関するルーティング */
-Route::get('/subject', [SubjectControoler::class, 'index'])->name('subjects');
-Route::get('/subject/add' ,[SubjectControoler::class, 'add']);
-Route::post('/subject/add' ,[SubjectControoler::class, 'create']);
-Route::get('/subject/{id}' ,[SubjectControoler::class, 'edit']);
-Route::post('/subject/{id}' ,[SubjectControoler::class, 'update']);
-Route::delete('/subject/{id}' ,[SubjectControoler::class, 'delete']);
+Route::get('/subject', [SubjectController::class, 'index'])->name('subjects');
+Route::get('/subject/add' ,[SubjectController::class, 'add']);
+Route::post('/subject/add' ,[SubjectController::class, 'create']);
+Route::get('/subject/{id}' ,[SubjectController::class, 'edit']);
+Route::post('/subject/{id}' ,[SubjectController::class, 'update']);
+Route::delete('/subject/{id}' ,[SubjectController::class, 'delete']);
 
 /* homeromes に関するルーティング */
-Route::get('/homeroom', [HomeroomControoler::class, 'index'])->name('homerooms');
-Route::get('/homeroom/add' ,[HomeroomControoler::class, 'add']);
-Route::post('/homeroom/add' ,[HomeroomControoler::class, 'create']);
-Route::get('/homeroom/{id}' ,[HomeroomControoler::class, 'edit']);
-Route::post('/homeroom/{id}' ,[HomeroomControoler::class, 'update']);
-Route::delete('/homeroom/{id}' ,[HomeroomControoler::class, 'delete']);
+Route::get('/homeroom', [HomeroomController::class, 'index'])->name('homerooms');
+Route::get('/homeroom/add' ,[HomeroomController::class, 'add']);
+Route::post('/homeroom/add' ,[HomeroomController::class, 'create']);
+Route::get('/homeroom/{id}' ,[HomeroomController::class, 'edit']);
+Route::post('/homeroom/{id}' ,[HomeroomController::class, 'update']);
+Route::delete('/homeroom/{id}' ,[HomeroomController::class, 'delete']);
 
 require __DIR__.'/auth.php';

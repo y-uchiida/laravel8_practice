a1a3d371fcf267702c1de6afe52e75eb4c8f18d3
y-uchiida <y.uchiida@nirneege.com>
2021-11-12 01:42:35 +0900

    set routings

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function (){ return (redirect('/student')); });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+/* students に関するルーティング */
+Route::get('/student', [StudentControoler::class, 'index'])->name('student');
+Route::get('/student/add', [StudentControoler::class, 'add']);
+Route::post('/student/add', [StudentControoler::class, 'create']);
+Route::post('/student/{id}', [StudentControoler::class, 'update']);
+Route::delete('/student/{id}', [StudentControoler::class, 'delete']);
+Route::get('/student/{id}', [StudentControoler::class, 'edit']);
+
+/* teachers に関するルーティング */
+Route::get('/teacher', [TeacherControoler::class, 'index'])->name('teachers');
+Route::get('/teacher/add',[TeacherControoler::class, 'add']);
+Route::post('/teacher/add',[TeacherControoler::class, 'create']);
+Route::get('/teacher/{id}' ,[TeacherControoler::class, 'edit']);
+Route::post('/teacher/{id}' ,[TeacherControoler::class, 'update']);
+Route::delete('/teacher/{id}' ,[TeacherControoler::class, 'delete']);
+
+/* subjects に関するルーティング */
+Route::get('/subject', [SubjectControoler::class, 'index'])->name('subjects');
+Route::get('/subject/add' ,[SubjectControoler::class, 'add']);
+Route::post('/subject/add' ,[SubjectControoler::class, 'create']);
+Route::get('/subject/{id}' ,[SubjectControoler::class, 'edit']);
+Route::post('/subject/{id}' ,[SubjectControoler::class, 'update']);
+Route::delete('/subject/{id}' ,[SubjectControoler::class, 'delete']);
+
+/* homeromes に関するルーティング */
+Route::get('/homeroom', [HomeroomControoler::class, 'index'])->name('homerooms');
+Route::get('/homeroom/add' ,[HomeroomControoler::class, 'add']);
+Route::post('/homeroom/add' ,[HomeroomControoler::class, 'create']);
+Route::get('/homeroom/{id}' ,[HomeroomControoler::class, 'edit']);
+Route::post('/homeroom/{id}' ,[HomeroomControoler::class, 'update']);
+Route::delete('/homeroom/{id}' ,[HomeroomControoler::class, 'delete']);
 
 require __DIR__.'/auth.php';
